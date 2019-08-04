29787b1f3d01ecb47f1414a2a59852049d611fa3
unknown <1danabana@gmail.com>
2019-08-03 17:40:08 -0700

    inserted giphy api into the web app

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ $(function(){
     populateButton(searchArray, 'searchButton', '#buttonsArea');
 })
 
-var searchArray = ["A", "B", "C"];
+var searchArray = ["terminator", "B", "C"];
 
 function populateButton(searchArray, classToAdd, areaToAddTo){
     $(areaToAddTo).empty();
@@ -17,12 +17,25 @@ function populateButton(searchArray, classToAdd, areaToAddTo){
 }
 $(document).on('click', '.searchButton', function(){
     var type = $(this).data('type');
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=75e52coobNBgUkl0m4wSPjcWsvg7bHZ4limit=12'
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=eHRibPgVWITxfreSKNqdNsX1VGj9ft2dlimit=12'
     $.ajax({url:queryURL,method:'GET'})
         .done(function(response){
-            for(var i= 0; i>response.data.length; i++){
+            for(var i = 0; i > response.data.length; i++){
                 var searchDiv = $('<div class="search-item">');
-                var rating 
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('Rating: ' + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                image.attr('src', still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
             }
         })
 })
+
